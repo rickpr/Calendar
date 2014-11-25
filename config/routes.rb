ae6517b1c94ae48b22c0b8b2b66837a5878b6b22
Ricardo Piro-Rael <fdisk@fdisk.co>
2014-11-24 18:10:40 -0700

    Working, about to delete posts

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root 'home#index'
   resource 'home'
   get '/kalendar' => 'kalendar#index', as: :kalendar
+  get '/logout' => 'home#logout'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
